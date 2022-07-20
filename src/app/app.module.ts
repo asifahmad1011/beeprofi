c1a3ca8a2e191a3e7a6c4f232f4fd32b819cb815
asifahmad1011 <asif.apag@gmail.com>
2022-07-20 17:54:36 +0200

    Initial structure with Nav Bar

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProjectsComponent } from './projects/projects.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutusComponent,
+    SignupComponent,
+    ProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  public homeP = false;
+  public servicesP = false;
+  public projectsP = false;
+  public aboutusP = false;
+  public signupP = false;
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+  }
+
+  homePage(){
+    this.homeP = true;
+    this.servicesP = false;
+    this.projectsP = false;
+    this.aboutusP = false;
+    this.signupP = false;
+    console.log("function called...")
+  }
+
+  servicesPage(){
+    this.homeP = false;
+    this.servicesP = true;
+    this.projectsP = false;
+    this.aboutusP = false;
+    this.signupP = false;
+    console.log("function called...")
+  }
+  projectsPage(){
+    this.homeP = false;
+    this.servicesP = false;
+    this.projectsP = true;
+    this.aboutusP = false;
+    this.signupP = false;
+    console.log("function called...")
+  }
+  aboutusPage(){
+    this.homeP = false;
+    this.servicesP = false;
+    this.projectsP = false;
+    this.aboutusP = true;
+    this.signupP = false;
+    console.log("function called...")
+  }
+  signupPage(){
+    this.homeP = false;
+    this.servicesP = false;
+    this.projectsP = false;
+    this.aboutusP = false;
+    this.signupP = true;
+    console.log("function called...")
+  }
+
+}
